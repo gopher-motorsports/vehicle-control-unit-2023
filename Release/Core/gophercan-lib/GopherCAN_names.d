4ffa364311f72bf5ae0ea1c4e2326009e3d906e7
Ben Abbott <babbott1000@gmail.com>
2022-12-28 09:10:44 -0600

    Removed ignored files

--- Release/Core/gophercan-lib/GopherCAN_names.d
+++
@@ -1,4 +0,0 @@
-Core/gophercan-lib/GopherCAN_names.o: \
- C:/Users/14695/STM32CubeIDE/workspace/gophercan-lib/GopherCAN_names.c \
- C:/Users/14695/STM32CubeIDE/workspace/gophercan-lib/GopherCAN_names.h
-C:/Users/14695/STM32CubeIDE/workspace/gophercan-lib/GopherCAN_names.h:
